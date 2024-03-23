30fcfd566f832b0d950b526d3212df5fa8d2db49
Mehedi Hasan Himel <mehedihasanhimel89@gmail.com>
2024-03-23 15:10:33 +0600

    internation route demo hotel image added

--- src/app/International/Morisas/page.tsx
+++ src/app/International/Morisas/page.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Hotel from "@/components/Hotel/Hotel";
 
-import destination from "@/data/domestic/SlyhetHotel.json";
-export default function Slyhet() {
+import destination from "@/data/international/internationalDemoHotel.json";
+export default function page() {
   return (
     <div className="mt-40">
-      <h2 className="text-center text-2xl font-semibold">Slyhet</h2>
+      <h2 className="text-center text-2xl font-semibold"> Hotel&#39;s</h2>
       <Hotel destination={destination} />
     </div>
   );
