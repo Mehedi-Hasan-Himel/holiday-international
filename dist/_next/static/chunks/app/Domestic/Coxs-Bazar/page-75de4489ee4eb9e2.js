705345733bae570b204e86e569b361d086eeacb7
Mehedi Hasan Himel <mehedihasanhimel89@gmail.com>
2024-03-23 15:21:35 +0600

    build

--- dist/_next/static/chunks/app/Domestic/Coxs-Bazar/page-75de4489ee4eb9e2.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[368,5797,8522,8959,722,6783,5794,6836,2002,3786,6949,2435,4181,2926,7346,8898,1983,1068,7295,9775,2322,2626],{63:function(n,e,u){Promise.resolve().then(u.t.bind(u,5250,23))}},function(n){n.O(0,[5250,2971,8069,1744],function(){return n(n.s=63)}),_N_E=n.O()}]);
