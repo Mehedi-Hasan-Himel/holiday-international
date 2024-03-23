6d789dad6848231a0285396ddce94138ba90082b
Mehedi Hasan Himel <mehedihasanhimel89@gmail.com>
2024-03-23 09:22:59 +0600

    international page

--- src/app/International/Vietnam/page.tsx
+++ src/app/International/Vietnam/page.tsx
@@ -1,20 +1,7 @@
 import React from "react";
 import Hotel from "@/components/Hotel/Hotel";
 
-const destination = [
-  {
-    id: 1,
-    name: "Rose View Hotel",
-    image: "https://i.ibb.co/1z3c4hB/Rose-View-Hotel.jpg",
-    href: "Rose-View-Hotel",
-  },
-  {
-    id: 2,
-    name: "Hotel Noorjahan Grand",
-    image: "https://i.ibb.co/8cGfhj8/Hotel-Noorjahan-Grand.jpg",
-    href: "Hotel-Noorjahan-Grand",
-  },
-];
+import destination from "@/data/domestic/SlyhetHotel.json";
 export default function Slyhet() {
   return (
     <div className="mt-40">
